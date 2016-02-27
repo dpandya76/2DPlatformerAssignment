49a597e6b26dab62d6f5112cca41fef07b9da83a
dpandya76 <dpandya76@gmail.com>
2016-02-27 17:56:13 -0500

    Added Physics to the hero and Collision detection

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    private int _scoreValue;
+    private int _livesValue;
+    
+
+    [SerializeField]
+    private AudioSource _stageclearSound;
+
+
+    // Public access methods for score and lives
+    public int ScoreValue
+    {
+        get
+        {
+            return this._scoreValue;
+        }
+
+        set
+        {
+
+            this._scoreValue = value;
+            if (this._scoreValue >= 10)
+            {
+                this._endGame();
+            }
+            else {
+                this.ScoreLabel.text = "Score:" + this._scoreValue;
+
+            }
+        }
+    }
+
+    public int LivesValue
+    {
+        get
+        {
+            return this._livesValue;
+        }
+
+        set
+        {
+            this._livesValue = value;
+            if (this._livesValue <= 0)
+            {
+                this._endGame();
+            }
+            else
+            {
+                this.LivesLabel.text = "Lives:" + this._livesValue;
+            }
+
+        }
+    }
+
+
+    // PUBLIC  INSTANCE VARIABLES
+    public int asteroidNumber = 4;
+    public AsteroidController asteroids;
+    public Text LivesLabel;
+    public Text ScoreLabel;
+    public Text InfoLabel;
+    public Text GameOverLabel;
+    public Text WinLabel;
+    public Text TargetLabel;
+    public Text TenTargetLabel;
+    public UFOController UFO;
+    public MapController map;
+    public Text HighScoreLabel;
+    public Button RestartButton;
+    
+
+    // Use this for initialization
+    void Start () {
+        this._initialize();
+       
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //private methods
+    private void _initialize()
+    {
+        this.ScoreValue = 0;
+        this.LivesValue = 5;
+        this.GameOverLabel.enabled = false;
+        this.HighScoreLabel.enabled = false;
+        
+        this.RestartButton.gameObject.SetActive(false);
+        this.InfoLabel.enabled = false;
+        this.WinLabel.enabled = false;
+        this.TargetLabel.enabled = false;
+        this.TenTargetLabel.enabled = true;
+        for (int asteroidCount=0; asteroidCount<this.asteroidNumber;asteroidCount++)
+        {
+            Instantiate(asteroids.gameObject);
+        }
+
+    }
+
+    private void _endGame()
+    {
+        this.HighScoreLabel.text = "High Score:" + this._scoreValue;
+        if(this._scoreValue<10)
+        {
+            this.GameOverLabel.enabled = true;
+            this.InfoLabel.enabled = true;
+            
+            
+        }
+        else
+        {
+            this.TargetLabel.enabled = true;
+            this.WinLabel.enabled = true;
+            
+        }
+        this._stageclearSound.Play();
+        this.UFO.gameObject.SetActive(false);
+        this.map.gameObject.SetActive(false);
+        this.LivesLabel.enabled = false;
+        this.ScoreLabel.enabled = false;
+        this.TenTargetLabel.enabled = false;
+        this.HighScoreLabel.enabled = true;
+        this.RestartButton.gameObject.SetActive(true);
+
+    }
+
+    public void RestartButtonClick()
+    {
+        Application.LoadLevel("Main");
+    }
+
+}
--- Assets/Scripts/HeroController.cs
+++ Assets/Scripts/HeroController.cs
@@ -1,42 +1,164 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class HeroController : MonoBehaviour {
 
+//velocity range utility class
+[System.Serializable]
+public class VelocityRange
+{
+    public float minimum;
+    public float maximum;
+
+    //constructor
+
+    public VelocityRange(float minimum, float maximum)
+    {
+        this.minimum = minimum;
+        this.maximum = maximum;
+
+    }
+}
+
+public class HeroController : MonoBehaviour {
+    //public instance variables
+    public VelocityRange velocityRange;
+    public float moveForce;
+    public float jumpForce;
+    public Transform groundCheck;
+    public Transform Camera;
+
+    // Private instance variables
     private Animator _animator;
     private float _move;
     private float _jump;
+    private bool _facingRight;
+    private Transform _transform;
+    private Rigidbody2D _rigidBody2D;
+    private bool _isGrounded;
 
 	// Use this for initialization
 	void Start () {
+        this.velocityRange = new VelocityRange(300f, 5000f);
+ 
+
+        this._transform = gameObject.GetComponent<Transform>();
         this._animator = gameObject.GetComponent<Animator>();
+        this._rigidBody2D = gameObject.GetComponent<Rigidbody2D>();
         this._move = 0f;
         this._jump = 0f;
-
+        this._facingRight = true;
+        //the hero placed in start postion
+        this.spwan();
+        
        
 	
 	}
 	
+
 	// Update is called once per frame
-	void Update () {
-        this._move = Input.GetAxis("Horizontal");
-        this._jump = Input.GetAxis("Vertical");
+	void FixedUpdate () {
 
-        //walking
-        if (this._move != 0)
+        Vector3 currentPosition = new Vector3(this._transform.position.x, this._transform.position.y, -10f);
+        this.Camera.position = currentPosition;
+        
+
+        this._isGrounded = Physics2D.Linecast(this._transform.position,
+                                                this.groundCheck.position,
+                                                    1<<LayerMask.NameToLayer("Ground"));
+
+        Debug.DrawLine(this._transform.position, this.groundCheck.position);
+
+
+        float forceX = 0f;
+        float forceY = 0f;
+
+        //absolute vaue for velocity
+        float absValX = Mathf.Abs(this._rigidBody2D.velocity.x);
+        float absValY = Mathf.Abs(this._rigidBody2D.velocity.y);
+
+       //ensure if grounded
+        if (this._isGrounded)
         {
-            this._animator.SetInteger("Anim_state", 1);
+            // gets a number between -1 to 1 for both Horizontal and Vertical Axes
+            this._move = Input.GetAxis("Horizontal");
+            this._jump = Input.GetAxis("Vertical");
+
+            if (this._move != 0)
+            {
+                if (this._move > 0)
+                {
+                    // movement force
+                    if (absValX < this.velocityRange.maximum)
+                    {
+                        forceX = this.moveForce;
+                    }
+                    this._facingRight = true;
+                    this._flip();
+                }
+                if (this._move < 0)
+                {
+                    // movement force
+                    if (absValX < this.velocityRange.maximum)
+                    {
+                        forceX = -this.moveForce;
+                    }
+                    this._facingRight = false;
+                    this._flip();
+                }
+
+                // call the walk clip
+                this._animator.SetInteger("Anim_state", 1);
+            }
+            else {
+
+                // set default animation state to "idle"
+                this._animator.SetInteger("Anim_state", 0);
+            }
+
+            if (this._jump > 0)
+            {
+                // jump force
+                if (absValY < this.velocityRange.maximum)
+                {
+                    //this._jumpSound.Play();
+                    forceY = this.jumpForce;
+                }
+
+            }
         }
-
-        else { 
-        if(this._jump>0)
-        {
+        else {
+            // call the "jump" clip
             this._animator.SetInteger("Anim_state", 2);
         }
-        
-            //idle
-            this._animator.SetInteger("Anim_state", 0);
-        }
 
+        // Apply the forces to the player
+        this._rigidBody2D.AddForce(new Vector2(forceX, forceY));
+    }
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Death"))
+        {
+            this.spwan();
+        }
+    }
+
+    //private methods
+    private void _flip()
+    {
+        if (this._facingRight)
+        {
+            this._transform.localScale= new Vector2(1, 1);
+        }
+        else
+        {
+            this._transform.localScale = new Vector2(-1, 1);
+        }
+    }
+
+    private void spwan()
+    {
+        this._transform.position = new Vector3(-126f, 126f, 0);
     }
 }
